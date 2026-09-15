0e5eccca2a644bda8ff9a19fbd86143da6ba58c3
Regstar2 <carkov195@gmail.com>
2026-09-15 17:05:12 +0300

    feat(awg): include AWG in route diagnostics labels

--- app/src/main/java/com/amurcanov/tgwsproxy/RoutePolicyDiagnostics.kt
+++ app/src/main/java/com/amurcanov/tgwsproxy/RoutePolicyDiagnostics.kt
@@ -113,6 +113,7 @@ object RoutePolicyDisplayNames {
                 RouteKind.DIRECT_WS -> R.string.route_policy_direct_ws
                 RouteKind.WORKER_WS -> R.string.route_policy_worker_ws
                 RouteKind.CF_PROXY_WS -> R.string.route_policy_cf_proxy_ws
+                RouteKind.AWG_WARP -> R.string.route_policy_awg_warp
                 RouteKind.TCP_FALLBACK -> R.string.route_policy_tcp_fallback
             },
         )
